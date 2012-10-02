311ae9166efdbe309d1a5d389dc7ef993d6cfee4
Victor Castell <victorcoder@gmail.com>
2012-10-02 18:19:20 +0200

    Basic invalidate

--- event.go
+++ event.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"time"
-	"regexp"
 	"errors"
 	"fmt"
+	"regexp"
+	"time"
 )
 
 import "labix.org/v2/mgo"
 import "labix.org/v2/mgo/bson"
 
 var (
-	typeRe = "^[a-z][a-zA-Z0-9_]+$"
-	knownByType = make(map[string]bool)
-	eventsToSaveByType = make(map[string][]bson.M)
-	timesToInvalidateByTierByType = make(map[string]string)
+	typeRe                        = "^[a-z][a-zA-Z0-9_]+$"
+	knownByType                   = make(map[string]bool)
+	eventsToSaveByType            = make(map[string][]Event)
+	timesToInvalidateByTierByType = make(map[string]map[time.Duration][]int64)
 )
 
 type Event struct {
-	Id string `json:"id"`
-	Type string `json:"type"`
-	Time time.Time `json:"time"`
-	Data map[string]interface{} `json:"data"`
+	Id   string                 `bson:"_id,omitempty" json:"id"`
+	Type string                 `bson:"" json:"type"`
+	Time time.Time              `bson:"t" json:"time"`
+	Data map[string]interface{} `bson:"d" json:"data"`
 }
 
 type Putter struct {
@@ -30,75 +30,71 @@ type Putter struct {
 
 func NewPutter(db *mgo.Database) *Putter {
 	return &Putter{db: db}
-}	
+}
 
-func (putr *Putter)Put(ev Event) error {
+func (putr *Putter) Put(event Event) error {
+	eventType := event.Type
+	event.Type = ""
+
 	// Validate the date and type.
-	if matched, _ := regexp.MatchString(typeRe, ev.Type); !matched {
+	if matched, _ := regexp.MatchString(typeRe, eventType); !matched {
 		return errors.New("invalid type")
 	}
 
-	fmt.Println(ev)
+	fmt.Println(event)
 
-	t := ev.Time
-	if t.IsZero() {
-		t = time.Now()
-	}
-
-	// If an id is specified, promote it to Mongo's primary key.
-	event := bson.M{"t": t, "d": ev.Data}
-	if ev.Id != "" {
-		event["_id"] = ev.Id
+	if event.Time.IsZero() {
+		event.Time = bson.Now()
 	}
 
 	// If this is a known event type, save immediately.
-    	if _, ok := knownByType[ev.Type]; ok {
-    		return putr.save(ev.Type, event)
-    	}
+	if _, ok := knownByType[eventType]; ok {
+		return putr.save(eventType, event)
+	}
 
-    	// If someone is already creating the event collection for this new type,
-    	// then append this event to the queue for later save.
-    	if _, ok := eventsToSaveByType[ev.Type]; ok {
-    		eventsToSaveByType[ev.Type] = append(eventsToSaveByType[ev.Type], event)
-    	}
+	// If someone is already creating the event collection for this new type,
+	// then append this event to the queue for later save.
+	if _, ok := eventsToSaveByType[eventType]; ok {
+		eventsToSaveByType[eventType] = append(eventsToSaveByType[eventType], event)
+	}
 
- 	// Otherwise, it's up to us to see if the collection exists, verify the
+	// Otherwise, it's up to us to see if the collection exists, verify the
 	// associated indexes, create the corresponding metrics collection, and save
 	// any events that have queued up in the interim!
 
 	// First add the new event to the queue.
-	eventsToSaveByType[ev.Type] = append(eventsToSaveByType[ev.Type], event)
+	eventsToSaveByType[eventType] = append(eventsToSaveByType[eventType], event)
 
 	// If the events collection exists, then we assume the metrics & indexes do
 	// too. Otherwise, we must create the required collections and indexes. Note
 	// that if you want to customize the size of the capped metrics collection,
 	// or add custom indexes, you can still do all that by hand.
- 	// Save any pending events to the new collection.
+	// Save any pending events to the new collection.
 	saveEvents := func() {
-		knownByType[ev.Type] = true
+		knownByType[eventType] = true
 
-		for _, eventToSave := range eventsToSaveByType[ev.Type] {
-			putr.save(ev.Type, eventToSave)
+		for _, eventToSave := range eventsToSaveByType[eventType] {
+			putr.save(eventType, eventToSave)
 		}
-		
-		delete(eventsToSaveByType, ev.Type)
+
+		delete(eventsToSaveByType, event.Type)
 	}
 
-    	names, _ := putr.db.CollectionNames()
-    	for _, name := range names {
-    		if name == ev.Type + "_events" {
-    			saveEvents()
-    			return nil
-    		}
-    	}
+	names, _ := putr.db.CollectionNames()
+	for _, name := range names {
+		if name == eventType+"_events" {
+			saveEvents()
+			return nil
+		}
+	}
 
-    	events := putr.db.C(ev.Type + "_events")
-    	// Events are indexed by time.
-      	events.EnsureIndex(mgo.Index{Key: []string{"t"}});
+	events := putr.db.C(eventType + "_events")
+	// Events are indexed by time.
+	events.EnsureIndex(mgo.Index{Key: []string{"t"}})
 
-      	// Create a capped collection for metrics. Three indexes are required: one
+	// Create a capped collection for metrics. Three indexes are required: one
 	// for finding metrics, one (_id) for updating, and one for invalidation.
-	metrics := putr.db.C(ev.Type + "_metrics")
+	metrics := putr.db.C(eventType + "_metrics")
 	err := metrics.Create(&mgo.CollectionInfo{Capped: true, MaxBytes: 1e7, ForceIdIndex: true})
 	if err != nil {
 		return err
@@ -107,7 +103,7 @@ func (putr *Putter)Put(ev Event) error {
 	metrics.EnsureIndex(mgo.Index{Key: []string{"i", "_id.l", "_id.t"}})
 	saveEvents()
 
-    	return nil
+	return nil
 }
 
 // Save the event of the specified type, and queue invalidation of any cached
@@ -118,8 +114,36 @@ func (putr *Putter)Put(ev Event) error {
 // delay between saving the event and invalidating the metrics reduces the
 // likelihood of a race condition between when the events are read by the
 // evaluator and when the newly-computed metrics are saved.
-func (put *Putter)save(eventType string, event bson.M) error {
+func (put *Putter) save(eventType string, event Event) error {
 	return put.db.C(eventType + "_events").Insert(event)
-	//queueInvalidation(type, event);
+	queueInvalidation(eventType, event)
+
+	return nil
 }
 
+func queueInvalidation(eventType string, event Event) error {
+	eventTime := event.Time
+
+	if timesToInvalidateByTier, ok := timesToInvalidateByTierByType[eventType]; ok {
+		for tier := range Tiers {
+			var tierTimes []int64 = timesToInvalidateByTier[tier]
+			tierTime := Tiers[tier].Floor(eventTime).Unix()
+			i := bisect(tierTimes, tierTime)
+
+			if i >= len(tierTimes) {
+				tierTimes = append(tierTimes, tierTime)
+			} else if tierTimes[i] > tierTime {
+				fmt.Println("**********")
+				fmt.Println(i)
+			}
+		}
+	} else {
+		timesToInvalidateByTierByType[eventType] = make(map[time.Duration][]int64)
+
+		for tier := range Tiers {
+			timesToInvalidateByTierByType[eventType][tier] = []int64{Tiers[tier].Floor(eventTime).Unix()}
+		}
+	}
+
+	return nil
+}
--- tiers.go
+++ tiers.go
@@ -6,56 +6,56 @@ import (
 
 var (
 	second10 = 10 * time.Second
-	minute5 = 5 * time.Minute
-	day = 24 * time.Hour
+	minute5  = 5 * time.Minute
+	day      = 24 * time.Hour
 )
 
 type Tier struct {
-	Key time.Duration
-	Floor func(d int64) time.Time
-	Ceil func(t Tier, date int64) time.Time
-	Step func(d int64) time.Time
-	Next *Tier
-	Size int
+	Key   time.Duration
+	Floor func(d time.Time) time.Time
+	Ceil  func(t Tier, date int64) time.Time
+	Step  func(d time.Time) time.Time
+	Next  *Tier
+	Size  int
 }
 
 func (t Tier) tierCeil(date int64) time.Time {
-  return t.Step(t.Floor(time.Unix(date - 1, 0).Unix()).Unix())
+	return t.Step(t.Floor(time.Unix(date-1, 0)))
 }
 
 var Tiers = map[time.Duration]Tier{
-	second10: Tier{ 
-		Key: second10,
-    		Floor: func(d int64) time.Time { return time.Unix(d / int64(second10) * int64(second10), 0) },
-    		Ceil: Tier.tierCeil,
-    		Step: func(d int64) time.Time { return time.Unix(+d + int64(second10), 0) },
+	second10: Tier{
+		Key:   second10,
+		Floor: func(d time.Time) time.Time { return time.Unix(d.Unix()/int64(second10)*int64(second10), 0) },
+		Ceil:  Tier.tierCeil,
+		Step:  func(d time.Time) time.Time { return time.Unix(+d.Unix()+int64(second10), 0) },
 	},
 	time.Minute: Tier{
-    		Key: time.Minute,
-    		Floor: func(d int64) time.Time { return time.Unix(d / int64(time.Minute) * int64(time.Minute), 0) },
-    		Ceil: Tier.tierCeil,
-    		Step: func(d int64) time.Time { return time.Unix(+d + int64(time.Minute), 0) },
+		Key:   time.Minute,
+		Floor: func(d time.Time) time.Time { return time.Unix(d.Unix()/int64(time.Minute)*int64(time.Minute), 0) },
+		Ceil:  Tier.tierCeil,
+		Step:  func(d time.Time) time.Time { return time.Unix(+d.Unix()+int64(time.Minute), 0) },
 	},
 	minute5: Tier{
-    		Key: minute5,
-    		Floor: func(d int64) time.Time { return time.Unix(d / int64(minute5) * int64(minute5), 0) },
-    		Ceil: Tier.tierCeil,
-    		Step: func(d int64) time.Time { return time.Unix(+d + int64(minute5), 0) },
+		Key:   minute5,
+		Floor: func(d time.Time) time.Time { return time.Unix(d.Unix()/int64(minute5)*int64(minute5), 0) },
+		Ceil:  Tier.tierCeil,
+		Step:  func(d time.Time) time.Time { return time.Unix(+d.Unix()+int64(minute5), 0) },
 	},
 	time.Hour: Tier{
-		Key: time.Hour,
-    		Floor: func(d int64) time.Time { return time.Unix(d / int64(time.Hour) * int64(time.Hour), 0) },
-    		Ceil: Tier.tierCeil,
-    		Step: func(d int64) time.Time { return time.Unix(+d + int64(time.Hour), 0) },
-    		//Next: &Tiers[minute5],
-    		Size: 12,
+		Key:   time.Hour,
+		Floor: func(d time.Time) time.Time { return time.Unix(d.Unix()/int64(time.Hour)*int64(time.Hour), 0) },
+		Ceil:  Tier.tierCeil,
+		Step:  func(d time.Time) time.Time { return time.Unix(+d.Unix()+int64(time.Hour), 0) },
+		//Next: &Tiers[minute5],
+		Size: 12,
 	},
 	day: Tier{
-    		Key: day,
-    		Floor: func(d int64) time.Time { return time.Unix(d / int64(day) * int64(day), 0) },
-    		Ceil: Tier.tierCeil,
-    		Step: func(d int64) time.Time { return time.Unix(+d + int64(day), 0) },
-    		//Next: &Tiers[time.Hour],
-    		Size: 24,
+		Key:   day,
+		Floor: func(d time.Time) time.Time { return time.Unix(d.Unix()/int64(day)*int64(day), 0) },
+		Ceil:  Tier.tierCeil,
+		Step:  func(d time.Time) time.Time { return time.Unix(+d.Unix()+int64(day), 0) },
+		//Next: &Tiers[time.Hour],
+		Size: 24,
 	},
 }
